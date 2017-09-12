21cf03279a9e5cab7849b54b793c46975f394b99
Dwight Spencer <dspencer@computekindustries.com>
2017-09-12 18:56:00 -0500

    (feat) Parallelized Builds with driver and overly support

--- contrib/swarm/cluster/create.sh
+++ contrib/swarm/cluster/create.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-provider=virtualbox
+
+provider=${MACHINE_DRIVER:-"virtualbox"}
+provider_ops=${MACHINE_OPS:-"--engine-storage-driver overlay2"}
+maxnodes=${NODES:-3}
+
 echo "### Init Servers ###"
 
-docker-machine create -d ${provider} sw1 &
-docker-machine create -d ${provider} sw2 &
-docker-machine create -d ${provider} sw3 &
-
+seq 1 ${maxnodes} | xargs -I{} -P${maxnodes} docker-machine create -d "${provider}" ${MACHINE_OPS} sw{}
 wait
 
 echo "### Configurate cluster ###"
@@ -13,8 +14,11 @@ echo "### Configurate cluster ###"
 MANAGER_IP=$(docker-machine ip sw1)
 docker-machine ssh sw1 docker swarm init --advertise-addr ${MANAGER_IP}
 TOKEN=$(docker-machine ssh sw1 docker swarm join-token -q worker)
-docker-machine ssh sw2 docker swarm join --token ${TOKEN} ${MANAGER_IP}:2377
-docker-machine ssh sw3 docker swarm join --token ${TOKEN} ${MANAGER_IP}:2377
+for x in seq 2 ${maxnodes}; do
+  ADDR=$(docker-machine ip sw${x})
+  
+  docker-machine ssh sw${x} docker swarm join --token ${TOKEN} --listen-addr ${ADDR} --advertise-addr ${ADDR} ${MANAGER_IP}:2377
+done
 
 # Information
 echo ""
